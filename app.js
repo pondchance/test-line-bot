4895c84d84647ab4f59535342a19aec4fe80372a
comsci <comsci@comscis-iMac.local>
2019-03-13 15:24:29 +0700

    /webhook

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var request = require('request')
 var app = express()
-var cors = require('cors')
 
-app.use(cors())
 app.use(bodyParser.json())
 
 app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-
-app.get('/', function (req, res) {
-	res.send('Hello')
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
+  }
+  res.sendStatus(200)
 })
+
+function sendText (sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer {WD0eIf28N5C0LsYDIZT2cr0O4i9RWvnWyWv6VGKcM0eO+WJCzmDWDX+dCp4ApU9U6zujpLevfyStWUpB9kJWWsG+2c/jrjvE+24anKD2TWfTuM2NCfcC/TY6AhIZxZ4k3EbXTsp0pARdkqG3z3afmwdB04t89/1O/w1cDnyilFU=}'
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
+}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
-})
-app.post('/webhook', (req, res) => {
-    res.sendStatus(200)
 })
